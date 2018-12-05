a01d34e57935eb154fbf9071ab197ca107860e6f
AlexMFV <alexmfv1@gmail.com>
2018-12-05 15:44:40 +0000

    Completed Day 2 (Python)

--- Day 2/Python/day2.py
+++ Day 2/Python/day2.py
@@ -29,8 +29,42 @@ def main():
 
         if val.count(3) > 0:
             three += 1
-
+            
     print(checkSum(two, three))
+    
+##  Part 2
+
+    file = open("../input.txt", "r")
+    
+    isEqual = False
+    idx = 0 
+    
+    for line in file:
+        file2 = open("../input.txt", "r")
+        for line2 in file2:
+            if line != line2:
+                idx, isEqual = getDifference(line, line2)
+                
+            if isEqual:
+                break
+                
+        if isEqual:
+            line = line[:idx] + line[idx+1:]
+            print(line)
+            break
+                
+def getDifference(line, line2):
+    for i in range(len(line)):
+        if line[i] != line2[i]:
+            line = line[:i] + line[i+1:]
+            line2 = line2[:i] + line2[i+1:]
+            
+            if line == line2:
+                return i, True
+            else:
+                return i, False
+                
+    return i, False
 
 def checkSum(two, three):
     return two * three
